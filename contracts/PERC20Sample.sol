cfa628ef01d8b15b10a06f2c4f6e840e9206ba40
variniase <variniaserrallisonuun2iflw@hotmail.com>
2024-10-03 08:38:54 +0000

    Initialised

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("tre", "tre") {}
+    constructor() PERC20("f37c160ed995285889521689e973da15dfaf47a440894b024018298f964dd1ce", "f37c160ed995285889521689e973da15dfaf47a440894b024018298f964dd1ce") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
